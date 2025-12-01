73de4d63f57e9111bc864da1a4dd19c22631eb69
Sahana M Diwatar <192903639+Sahana105@users.noreply.github.com>
2025-12-01 23:25:36 +0530

    Refactor user schema to include isAdmin and validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    cartData: { type: Object, default: {} }
-}, { minimize: false })
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "Invalid email"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const userModel = mongoose.models.user || mongoose.model('user',userSchema);
-
-export default userModel
+export default mongoose.model("User", userSchema); // collection: "users"
