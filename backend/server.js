49e71fb39350c435207d537453c456bbf45b49ad
Sahana M Diwatar <192903639+Sahana105@users.noreply.github.com>
2025-12-01 23:55:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET","POST","PUT","DELETE"],
-    allowedHeaders: ["Content-Type","Authorization"]
+    allowedHeaders: ["Content-Type","Authorization","token"]
   })
 );
 
