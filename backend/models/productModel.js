97f1395575dcdda74773de617b49c6c9f5b984a2
Sahana M Diwatar <192903639+Sahana105@users.noreply.github.com>
2025-12-01 23:24:54 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
     price: { type: Number, required: true },
-    image: { type: Array, required: true },
-    category: { type: String, required: true },
-    subCategory: { type: String, required: true },
-    sizes: { type: Array, required: true },
-    bestseller: { type: Boolean },
-    date: { type: Number, required: true }
-})
+    image: { type: String },
+    category: { type: String },
+    inStock: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
 
-const productModel  = mongoose.models.product || mongoose.model("product",productSchema);
-
-export default productModel
+export default mongoose.model("Product", productSchema); // collection: "products"
