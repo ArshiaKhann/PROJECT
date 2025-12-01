ade50734cfa8a5b5f5d6381e30145d45bdb7325b
Sahana M Diwatar <192903639+Sahana105@users.noreply.github.com>
2025-12-01 23:33:05 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 import validator from "validator";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: [validator.isEmail, "Invalid email"],
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+    validate: {
+      validator: (value) => /\d/.test(value), // must contain at least 1 number
+      message: "Password must contain at least one number",
     },
   },
-  { timestamps: true }
-);
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true });
 
-export default mongoose.model("User", userSchema); // collection: "users"
+export default mongoose.model("User", userSchema);
