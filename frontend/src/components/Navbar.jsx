a73d09cbdc34829a860efe4170bc0e19ee733d58
Arshia Khan <arshiakhan.coral@gmail.com>
2025-12-29 20:27:12 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       
-      <Link to='/'><img src={assets.logo} className='w-70' alt="" /></Link>
+      <Link to='/'><img src={assets.logo} className='w-80' alt="" /></Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         
