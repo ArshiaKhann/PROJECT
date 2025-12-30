03da0ef219b649b4545b3f83fc30266edc56caac
Arshia Khan <arshiakhan.coral@gmail.com>
2025-12-30 21:16:13 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import User from "../models/User.js"; // make sure User.js exists in models folder
+import User from "../models/userModel.js";
 
 const router = express.Router();
 
@@ -9,16 +9,13 @@ router.post("/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create new user
     const newUser = await User.create({
       name,
       email,
